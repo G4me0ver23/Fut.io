e48e5f2ca9a18a5cc72bfa637f90dea4aafa1d61
G4me0ver23 <109762690+G4me0ver23@users.noreply.github.com>
2022-12-25 18:51:35 -0300

    Update DadosCadastro.js

--- src/components/DadosCadastro.js
+++ src/components/DadosCadastro.js
@@ -1,1 +1,15 @@
-
+import styles from "../styles/LoginTela.module.css";
+import React from "react";
+import DadosLogin from "./DadosLogin";
+function DadosCadastro(){
+    return(
+        <div className={styles.DadosLogin}>
+            <h3>Nome:</h3>
+            <input type="text" placeholder="Reginaldo da Silva Santos" />
+            <DadosLogin />
+            <h3>Confirmar senha:</h3>
+            <input type="password" />
+        </div>
+    )
+}
+export default DadosCadastro;
